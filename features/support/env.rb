17997892e4175b33f23e032d665afd3e9b3f061a
Emerson Teshima <emerson_t2007@hotmail.com>
2022-06-20 21:15:24 -0300

    alter

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,8 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'pry'
 
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome
+    config.app_host = "http://www.google.com.br"
+    config.default_max_wait_time = 10
+end
