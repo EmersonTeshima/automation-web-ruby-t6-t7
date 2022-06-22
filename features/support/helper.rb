f531055dfc6ef9a637ce611eea5f7e00a397e444
Emerson Teshima <emerson_t2007@hotmail.com>
2022-06-21 21:13:05 -0300

    Config hooks, helper file

---
+++ features/support/helper.rb
@@ -0,0 +1,13 @@
+module helper
+    def pritn_screen(file_name, result)
+        date = "#{Time.now.srtftime("%Y/%B/%D")}"
+        hour = "#{Time.now.srtftime("%H-%M-%S")}"
+
+        file_path = "reports/screenshots/tests_#{result}"
+        screenshots = "#{file_path}/#{date}/#{hour}/#{file_name}.png"
+        page.save_screenshot(screenshots)
+        attach(screenshots, 'image/png')
+    end
+end
+
+
